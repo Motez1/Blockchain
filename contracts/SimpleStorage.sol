b97b043f0afb5ccdfd0752c00d85f33616779ad8
Remix Workspace <>
2022-10-06 00:17:41 +0100

    Second smart contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+
+// A simple smart contract that allows to store the state 
+// of a balance variable, add and subtract an amount from it
+
 contract SimpleStorage {
     uint256 public MotezBalance;
+
+    function setBalance(uint256 _amount) public {
+        MotezBalance = _amount;
+    }
 
     function addToBalance(uint256 _amount) public {
         MotezBalance += _amount;
@@ -10,5 +18,9 @@ contract SimpleStorage {
 
     function subFromBalance(uint256 _amount) public {
         MotezBalance -= _amount;
+    }
+
+    function getBalance() public view returns (uint256){
+        return MotezBalance;
     }
 }
