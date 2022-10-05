7e68b9dea9ce1c33496e1d5a197e43e6bd8dbc0d
Remix Workspace <>
2022-10-06 00:05:04 +0100

    initial contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 contract HelloWorld {
     function printHelloWorld() public pure returns(string memory) {
-        return "Hello World, this is my first smart contract folks";
+        return "Hello World, this is my first smart contract folk";
     }
 }
